9b21d38467a07075b8a1b28913975709c4622bbb
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-07-05 10:37:47 +0100

    fix: update sitemap URLs to reflect domain change

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,13 +3,13 @@ import type { MetadataRoute } from "next";
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://open-designers.com",
+      url: "https://open-designers.org",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 1.0,
     },
     {
-      url: "https://open-designers.com/privacy",
+      url: "https://open-designers.org/privacy",
       lastModified: new Date(),
       changeFrequency: "yearly",
       priority: 0.5,
