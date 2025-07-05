5dd5ffcd4e986db685596707c7a3c8178cc03632
Daniel Olowoniyi <olowoniyidaniel@gmail.com>
2025-07-05 10:41:50 +0100

    fix: update robots.txt rules for Facebook crawler and change sitemap URL to new domain

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,11 +2,17 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-      disallow: "/private/",
-    },
-    sitemap: "https://open-designers.com/sitemap.xml",
+    rules: [
+      {
+        userAgent: "facebookexternalhit",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: "/private/",
+      },
+    ],
+    sitemap: "https://open-designers.org/sitemap.xml",
   };
 }
